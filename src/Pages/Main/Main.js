9890cb72887bf82fab3f2ec36157a92fa01d5a3a
gus4214 <98795239+gus4214@users.noreply.github.com>
2022-07-20 11:38:16 +0900

    Add: initial settings complete

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Map from '../Map/Map';
+
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Map/Map.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Map.scss';
+
+const Map = () => {
+  return <div>안녕하세요</div>;
+};
+
+export default Map;
